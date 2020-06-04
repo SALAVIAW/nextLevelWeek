0a96b4479f0ff33c31e0939e83d2712dcef7de53
tadeo <salaviawtadeo@gmail.com>
2020-06-04 21:58:30 +0100

    create colect items

---
+++ scripts/create-point.js
@@ -0,0 +1,65 @@
+function populateUFs () {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+        for (const state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}` 
+        }
+    })
+}
+
+populateUFs()
+
+function getCities(event){
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+       
+
+        for (const city of cities) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}` 
+        }
+
+        citySelect.disabled = false
+    })
+}
+
+
+document
+.querySelector("select[name=uf]")
+.addEventListener("change", getCities)
+
+
+// itens de coleta
+// pegando todos os itens dentro de li
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+function handleSelectedItem (event) {
+    const itemLi = event.target
+
+    //add or remove class in JavaScript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+    console.log()
+}
