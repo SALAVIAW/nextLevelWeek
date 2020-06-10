99295897c49646acfd207596da31d0418911b093
tadeo <salaviawtadeo@gmail.com>
2020-06-10 22:20:37 +0100

    end of 4th class

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const server = express()
 
+// Configurar pasta publica
+server.use(express.static("public"))
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", { // primeiro argumento LOCAL DA PASTA DOS ARQUIVOS HTML, segunda argumente um eh um OBJETO - primeira propriedade qual eh o servidor EXPRESS, e a segunda propriedade NOCACHE
+    express: server,
+    noCache: true
+})
+
 
 // configurar caminhos da minha aplicacao
 //pagina inicial
 //req: Requisicao and res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/nlw/src/views/index.html")
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
